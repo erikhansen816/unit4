7fc25fd06a6ecd1c69ba4d6f11c28a3803ec0fca
erikhansen816 <31485788+erikhansen816@users.noreply.github.com>
2017-10-18 13:51:31 -0400

    Updated from Brython Server: 10/18/2017 1:51:29 PM

--- printSquares.py
+++ printSquares.py
@@ -1,1 +1,11 @@
+#Erik Hansen
+#10/18/2017
+#10/18/2017
 
+def printSquares(num1,num2):
+    for i in range(1,num1+1):
+        print('+ - - '*num2+'+')
+        print('|     '*num2+'|')
+    print('+ - - '*num2+'+')
+    
+printSquares(4,4)
