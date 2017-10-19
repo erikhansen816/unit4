c48d7560c2e42ec4c6569b6e760ff90468700d86
erikhansen816 <31485788+erikhansen816@users.noreply.github.com>
2017-10-19 14:29:31 -0400

    Updated from Brython Server: 10/19/2017 2:29:28 PM

--- monkeyBanana.py
+++ monkeyBanana.py
@@ -5,9 +5,18 @@
 from ggame import *
 
 #constants
-ROWS = 23
-COLS = 45
+ROWS = 22
+COLS = 44
 CELL_SIZE = 20
+
+def moveRight(event):
+    monkey.x += CELL_SIZE
+def moveLeft(event):
+    monkey.x -= CELL_SIZE
+def moveUp(event):
+    monkey.y -= CELL_SIZE
+def moveDown(event):
+    monkey.y += CELL_SIZE
 
 if __name__ == '__main__':
     
@@ -24,4 +33,7 @@ if __name__ == '__main__':
     Sprite(bananaBox,(COLS*CELL_SIZE/2,ROWS*CELL_SIZE/2))
     
     App().listenKeyEvent('keydown','right arrow',moveRight)
+    App().listenKeyEvent('keydown','left arrow',moveLeft)
+    App().listenKeyEvent('keydown','up arrow',moveUp)
+    App().listenKeyEvent('keydown','down arrow',moveDown)
     App().run()
