5661272841fad2c7d5bde485bdb3653cf896d7f7
erikhansen816 <31485788+erikhansen816@users.noreply.github.com>
2017-10-25 13:40:40 -0400

    Update stringIntersect.py

--- stringIntersect.py
+++ stringIntersect.py
@@ -1,1 +1,17 @@
+#Erik Hansen
+#10/25/2017
+#stringIntersect.py
 
+def stringIntersect(word1,word2):
+    totalStr = ''
+    totalStr += word1
+        
+    totalStr +=word2
+    finalStr = ''
+    for ch in totalStr:
+        if ch in word1 and ch in word2:
+            if not ch in finalStr:
+                finalStr += ch
+    return finalStr
+    
+print(stringIntersect('mississippi','pennsylvania'))
