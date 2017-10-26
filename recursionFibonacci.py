7c55bc81260eb16fd2305b3f783addf295e7af6a
erikhansen816 <31485788+erikhansen816@users.noreply.github.com>
2017-10-26 14:42:58 -0400

    Updated from Brython Server: 10/26/2017 2:42:49 PM

--- recursionFibonacci.py
+++ recursionFibonacci.py
@@ -1,1 +1,11 @@
+#Erik Hansen
+#10/26/17
+#recursionFibonacci.py - A recursive version of the fibonacci function
 
+def fibonacci(n):
+    if n == 1 or n == 2:
+        return 1
+    else:
+        return fibonacci(n-1)+fibonacci(n-2)
+
+print(fibonacci(10))
