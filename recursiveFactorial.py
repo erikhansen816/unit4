f5b0cce4385d0bcee9a5599e65114d26b3323901
erikhansen816 <31485788+erikhansen816@users.noreply.github.com>
2017-10-26 14:33:20 -0400

    Updated from Brython Server: 10/26/2017 2:33:11 PM

--- recursiveFactorial.py
+++ recursiveFactorial.py
@@ -1,1 +1,12 @@
+#Erik Hansen
+#10/26/17
+#recursionFactorial.py - A recursive version of the factorial function
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return (n*factorial(n-1))
+    
+print(factorial(4))
 
