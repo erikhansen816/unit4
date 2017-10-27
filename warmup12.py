19a64fa799335dd43b2872d9429884617651e39f
erikhansen816 <31485788+erikhansen816@users.noreply.github.com>
2017-10-27 14:27:20 -0400

    Updated from Brython Server: 10/27/2017 2:27:19 PM

--- warmup12.py
+++ warmup12.py
@@ -1,1 +1,12 @@
+#Erik Hansen
+#10/27/2017
+#warmup12.py greatest common factor
 
+def GCF(num1,num2):
+    n = min(num1,num2)
+    for i in range(n+1,0,-1):
+        if num1%i == 0 and num2%i == 0:
+            return i
+    return i
+
+print(GCF(14,7))
