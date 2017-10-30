6bc2f1e8a7ef5b806de9c1c8e8b1ac1ef1dc9ce3
erikhansen816 <31485788+erikhansen816@users.noreply.github.com>
2017-10-30 14:29:20 -0400

    Updated from Brython Server: 10/30/2017 2:29:18 PM

--- quiz4.py
+++ quiz4.py
@@ -1,1 +1,17 @@
+#Erik Hansen
+#10/30/2017
+#quiz4.py
+
+def csia():
+    for i in range(1,6):
+        print('computer science is awesome')
+    
+def average(num1,num2,num3):
+    return ((num1+num2+num3)/3)
+
+def lastLetter(word):
+    
+    
+csia()
+print(average(2,4,6))
 
