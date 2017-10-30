f5a3bc76f806fc2e25db9da3997a64aa1461a1e2
erikhansen816 <31485788+erikhansen816@users.noreply.github.com>
2017-10-30 14:38:57 -0400

    Updated from Brython Server: 10/30/2017 2:38:54 PM

--- quiz4.py
+++ quiz4.py
@@ -10,8 +10,18 @@ def average(num1,num2,num3):
     return ((num1+num2+num3)/3)
 
 def lastLetter(word):
+    total = 0
+    for ch in word:
+        total = total + 1
+    if total == len(word):
+        print(ch)
     
-    
+def same(x,y):
+    if x == y:
+        return True
+    else:
+        return False
 csia()
 print(average(2,4,6))
-
+lastLetter('Lit')
+print(same(2*3,7-1))
